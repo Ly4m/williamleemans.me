9dd1c5f6e7fa726b155df36d1042d34c20d7f700
Liam <will.leemans@gmail.com>
2025-08-10 23:48:16 +0200

    feat: kickstart SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+import sitemap from '@astrojs/sitemap';
 
-  adapter: netlify()
+export default defineConfig({
+    site: 'https://williamleemans.me',
+    integrations: [sitemap()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    adapter: netlify(),
 });
