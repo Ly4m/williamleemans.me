79fc16c1736c4e4eec3753d54be309ad96dbcd1c
William Leemans <will.leemans@gmail.com>
2025-11-05 20:44:38 +0100

    feat: adds animated ember background

---
+++ src/components/EmberBackground.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+  import { EmberParticleSystem } from "../lib/ParticleSystem.ts";
+
+  let canvas: HTMLCanvasElement;
+  let system: EmberParticleSystem;
+
+  onMount(() => {
+    system = new EmberParticleSystem(canvas);
+    system.start();
+  });
+
+  onDestroy(() => {
+    system?.stop();
+  })
+</script>
+
+<canvas bind:this={canvas} class="-z-1 fixed"></canvas>
